b50d94ce99ef7e5b0df9f28b0621cbce675c7b6e
mela106 <meladong@gmail.com>
2020-10-29 17:53:57 +0000

    changes 2

--- some code.R
+++ some code.R
@@ -1,4 +1,4 @@
 # git config --global user.email meladong@gmail.com
 # git config --global user.name "mela106"
 # git config --global user.name
-# ns20200625.dta
+# ns20200625.dta (add to gitignore)
